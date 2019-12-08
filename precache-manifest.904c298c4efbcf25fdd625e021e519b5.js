88c2ce47147d2fad918d7d560555d8c3078a87b6
Erick <erick.hx22@gmail.com>
2019-12-08 11:41:53 -0500

    Updates

---
+++ precache-manifest.904c298c4efbcf25fdd625e021e519b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd99c72e094ef3411f800794ffbc5c4",
+    "url": "/Vikings-App/index.html"
+  },
+  {
+    "revision": "7be80294076febd642a9",
+    "url": "/Vikings-App/static/css/main.b08d7148.chunk.css"
+  },
+  {
+    "revision": "25156e9b1d04c7c39895",
+    "url": "/Vikings-App/static/js/2.716fc751.chunk.js"
+  },
+  {
+    "revision": "7be80294076febd642a9",
+    "url": "/Vikings-App/static/js/main.93d7419f.chunk.js"
+  },
+  {
+    "revision": "2fadc6b703a38a16fe08",
+    "url": "/Vikings-App/static/js/runtime-main.a80d1457.js"
+  }
+]);
